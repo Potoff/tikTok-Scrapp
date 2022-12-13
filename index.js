5b0d3d87686c0930af8083d10d23a6a9e9c5cf6b
Potoff <services.ankaamusic@gmail.com>
2022-12-13 22:37:16 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+// const axios = require("axios");
+
+// const options = {
+//   method: 'GET',
+//   url: 'https://scraptik.p.rapidapi.com/music-posts',
+//   params: {music_id: '7049497828150102790', cursor: '0'},
+//   headers: {
+//     'X-RapidAPI-Key': '2c1f31e216mshbcc7d4817f90aecp1039e2jsnde40d3cb2896',
+//     'X-RapidAPI-Host': 'scraptik.p.rapidapi.com'
+//   }
+// };
+
+// axios.request(options).then(function (response) {
+// 	console.log(response.data);
+// }).catch(function (error) {
+// 	console.error(error);
+// });
+
+
+
