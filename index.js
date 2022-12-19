078f3ea908a2607d65b70757ffb9d532340a60c7
Potoff <services.ankaamusic@gmail.com>
2022-12-19 19:27:37 +0100

    add sort by like

--- index.js
+++ index.js
@@ -1,20 +1,46 @@
-// const axios = require("axios");
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "2c1f31e216mshbcc7d4817f90aecp1039e2jsnde40d3cb2896",
+    "X-RapidAPI-Host": "scraptik.p.rapidapi.com",
+  },
+};
 
-// const options = {
-//   method: 'GET',
-//   url: 'https://scraptik.p.rapidapi.com/music-posts',
-//   params: {music_id: '7049497828150102790', cursor: '0'},
-//   headers: {
-//     'X-RapidAPI-Key': '2c1f31e216mshbcc7d4817f90aecp1039e2jsnde40d3cb2896',
-//     'X-RapidAPI-Host': 'scraptik.p.rapidapi.com'
-//   }
-// };
-
-// axios.request(options).then(function (response) {
-// 	console.log(response.data);
-// }).catch(function (error) {
-// 	console.error(error);
-// });
-
-
+document.getElementById("refresh").addEventListener("click", () => {
+  fetch(
+    "https://scraptik.p.rapidapi.com/music-posts?music_id=7049497828150102790&count=18&cursor=0",
+    options
+  ).then(function (response) {
+    response
+      .json()
+      .then((response) => {
+        tbody.innerHTML = "";
+        response.aweme_list.sort((a, b) => {
+          return parseFloat(b.statistics.digg_count) - parseFloat(a.statistics.digg_count)          
+          }
+        );
+        response.aweme_list.forEach((post) => {
+          let row = document.createElement("tr");
+          let tbody = document.getElementById("tbody");
+          let th = document.createElement("th");
+          let uniqueId = document.createElement("td");
+          let desc = document.createElement("td");
+          let like = document.createElement("td");
+          th.setAttribute("scope", "row");
+          th.innerText = post.author.nickname;
+          uniqueId.innerText = post.author.unique_id;
+          desc.innerText = post.desc;
+          like.innerText = post.statistics.digg_count;
+          row.appendChild(th);
+          row.appendChild(uniqueId);
+          row.appendChild(desc);
+          row.appendChild(like);
+          tbody.appendChild(row);
+        });
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  });
+});
 
